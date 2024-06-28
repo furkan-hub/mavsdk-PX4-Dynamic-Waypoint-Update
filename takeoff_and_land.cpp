27ebf7a060d62700f09ddda08e828974ad855778
Furkan Doğan <72547366+furkan-hub@users.noreply.github.com>
2024-06-28 16:39:56 +0300

    final version of route planning

--- takeoff_and_land.cpp
+++ takeoff_and_land.cpp
@@ -1,9 +1,12 @@
+//aisoft biyoteksan ship opperation mavsdk application
+// 2024 Muhammed Furkan Doğan
+
 #include <chrono>
 #include <cstdint>
 #include <mavsdk/mavsdk.h>
 #include <mavsdk/plugins/action/action.h>
 #include <mavsdk/plugins/telemetry/telemetry.h>
-#include <mavsdk/plugins/mission/mission.h>
+#include <mavsdk/plugins/mission_raw/mission_raw.h>
 #include <mavsdk/plugins/mavlink_passthrough/mavlink_passthrough.h>
 #include <iostream>
 #include <future>
@@ -11,6 +14,7 @@
 #include <thread>
 #include <cmath>
 #include <vector>
+#include <iomanip> // For std::setprecision
 #include "coordinates.h"
 
 using namespace mavsdk;
@@ -20,35 +24,36 @@ using chrono::seconds;
 using this_thread::sleep_for;
 
 // Home pozisyonu ayarlamak için gerekli değişkenler
-float home_latitude = 47.3977419;
-float home_longitude = 8.2455938;
-float home_altitude = -480;
-float drone2_pos[2];
+double home_latitude = 47.3977419;
+double home_longitude = 8.2455938;
+float home_altitude = 0;
+double drone2_pos[2];
 float drone2_attitude[3];
-float distance_treshold = 10;
+float distance_treshold = 60;
 float distance_diff = 0;
-float drone1_homepos[2];
+double drone1_homepos[2];
 float bearing = 0;
 
 // -180 +180 derece olan yaw açısını 0-360 arasında normalize eder
 double normalizeAngle(double angle) {
-    // 360 dereceye göre mod alarak normalize et
     angle = fmod(angle, 360.0);
-
-    // Negatif değerleri pozitife çevir
     if (angle < 0) {
         angle += 360.0;
     }
-
     return angle;
 }
-
+//radian dereceye çevirme
 float radian_to_degree(float radian) {
     return radian * (180.0 / M_PI);
 }
 
+//degree to radian
+float degree_to_radian(float degree) {
+    return degree * (M_PI / 180.0);
+}
+
 // Home noktası güncelleme fonksiyonu
-void update_home(MavlinkPassthrough& mavlink_passthrough, float home_latitude, float home_longitude, float home_altitude) {
+void update_home(MavlinkPassthrough& mavlink_passthrough, double home_latitude, double home_longitude, float home_altitude) {
     MavlinkPassthrough::CommandLong command{};
     command.target_sysid = mavlink_passthrough.get_target_sysid();
     command.target_compid = mavlink_passthrough.get_target_compid();
@@ -57,41 +62,50 @@ void update_home(MavlinkPassthrough& mavlink_passthrough, float home_latitude, f
     command.param2 = 0; // Unused
     command.param3 = 0; // Unused
     command.param4 = 0; // Unused
-    command.param5 = home_latitude;
-    command.param6 = home_longitude;
+    command.param5 = home_latitude; // converting to int32 required by MAVLink
+    command.param6 = home_longitude; // converting to int32 required by MAVLink
     command.param7 = home_altitude;
 
     mavlink_passthrough.send_command_long(command);
 
-    cout << "Home noktası güncellendi.\n";
+    cout << "Home noktası güncellendi: " << home_latitude << ", " << home_longitude << endl;
 }
 
-void update_waypoints(Mission& mission, vector<Mission::MissionItem>& waypoints, double distance_diff, double bearing) {
+void update_waypoints(MissionRaw& mission_raw, vector<MissionRaw::MissionItem>& waypoints,vector<double> drone2_pos_vector,vector<double> drone1_homepos_vector) {
     vector<pair<double, double>> waypoints_coords;
 
-    // Waypoint'lerin koordinatlarını al
+    distance_diff = haversine_distance(drone2_pos_vector, drone1_homepos_vector); // home noktası ve hareketli platform arası mesafe
+    bearing = calculate_bearing(drone2_pos_vector, drone1_homepos_vector); // bearing açısı hesabı
+
+    cout << distance_diff * 1000 << " Meter" << endl;
+    cout << bearing << " degrees" << endl;
+    //float bearing_radian = degree_to_radian(bearing);
+
+    // Waypoint'lerin koordinatlarını al ve int'ten double'a çevir
     for (const auto& wp : waypoints) {
-        waypoints_coords.emplace_back(wp.latitude_deg, wp.longitude_deg);
+        waypoints_coords.emplace_back(wp.x / 1e7, wp.y / 1e7); // converting from int to double
     }
 
-    // Her waypoint'i bağımsız olarak güncelle
+    // Her waypoint güncelle
     for (size_t i = 0; i < waypoints_coords.size(); ++i) {
         auto [x, y] = waypoints_coords[i];
         vector<double> point = {x, y};
 
         // Waypoint'in kendi konumuna göre dönüşüm uygula
         vector<double> new_coords = translate_coordinates(point, bearing, distance_diff);
+        cout << "waypoint " << i << ": " << fixed << setprecision(8) <<point[0] << ", " << point[1] << endl;
 
-        waypoints[i].latitude_deg = new_coords[0];
-        waypoints[i].longitude_deg = new_coords[1];
+
+        waypoints[i].x = static_cast<int32_t>(new_coords[0] * 1e7); // converting back to int32
+        waypoints[i].y = static_cast<int32_t>(new_coords[1] * 1e7); // converting back to int32
+
+        // Debug: Yeni koordinatları yazdır
+        cout << "Güncellenmiş waypoint " << i << ": " << fixed << setprecision(8) << static_cast<int32_t>(new_coords[0] * 1e7) << ", " << static_cast<int32_t>(new_coords[1] * 1e7) << endl;
     }
 
-    Mission::MissionPlan mission_plan;
-    mission_plan.mission_items = waypoints;
+    auto upload_result = mission_raw.upload_mission(waypoints);
 
-    auto upload_result = mission.upload_mission(mission_plan);
-
-    if (upload_result != Mission::Result::Success) {
+    if (upload_result != MissionRaw::Result::Success) {
         cerr << "Waypoint güncellenirken hata: " << upload_result << '\n';
     } else {
         cout << "Waypointler başarıyla güncellendi.\n";
@@ -113,11 +127,10 @@ int main() {
         return 1;
     }
 
-    // Wait until systems are found
+    // Sistemler bulunana kadar bekle
     this_thread::sleep_for(chrono::seconds(2));
 
     auto systems = mavsdk.systems();
-
     if (systems.size() < 2) {
         cerr << "Yeterli sayıda sistem bulunamadı.\n";
         return 1;
@@ -139,8 +152,8 @@ int main() {
     Action action1(system1);
     Action action2(system2);
 
-    Mission mission1(system1);
-    Mission mission2(system2);
+    MissionRaw mission_raw1(system1);
+    MissionRaw mission_raw2(system2);
 
     MavlinkPassthrough mavlink_passthrough1(system1);
     MavlinkPassthrough mavlink_passthrough2(system2);
@@ -158,11 +171,11 @@ int main() {
         return 1;
     }
 
-    // Hedef gemi için posizyon bilgisi yayını
+    // Hedef gemi için pozisyon bilgisi yayını
     telemetry2.subscribe_position([](Telemetry::Position position) {
-        cout << "Drone 2 - Yükseklik: " << position.relative_altitude_m << " m, "
-             << "Enlem: " << position.latitude_deg << " derece, "
-             << "Boylam: " << position.longitude_deg << " derece" << endl;
+        // cout << "Drone 2 - Yükseklik: " << position.relative_altitude_m << " m, "
+        //      << "Enlem: " << fixed << setprecision(8) << position.latitude_deg << " derece, "
+        //      << "Boylam: " << position.longitude_deg << " derece" << endl;
         drone2_pos[0] = position.latitude_deg;
         drone2_pos[1] = position.longitude_deg;
     });
@@ -175,19 +188,21 @@ int main() {
         drone2_attitude[0] = roll_deg;
         drone2_attitude[1] = pitch_deg;
         drone2_attitude[2] = yaw_deg;
-        cout << "Drone 2 - Roll: " << drone2_attitude[0] << " derece, "
-             << "Pitch: " << drone2_attitude[1] << " derece, "
-             << "Yaw: " << drone2_attitude[2] << " derece" << endl;
+        // cout << "Drone 2 - Roll: " << fixed << setprecision(2) << drone2_attitude[0] << " derece, "
+        //      << "Pitch: " << drone2_attitude[1] << " derece, "
+        //      << "Yaw: " << drone2_attitude[2] << " derece" << endl;
     });
 
     // Drone 1 home pozisyonunu alma
     telemetry1.subscribe_home([](Telemetry::Position home_position) {
-        cout << "Drone 1 Home - Enlem: " << home_position.latitude_deg
-             << ", Boylam: " << home_position.longitude_deg
-             << ", Yükseklik: " << home_position.relative_altitude_m << " m" << endl;
+        // cout << "Drone 1 Home - Enlem: " << fixed << setprecision(8) << home_position.latitude_deg
+        //      << ", Boylam: " << home_position.longitude_deg
+        //      << ", Yükseklik: " << home_position.relative_altitude_m << " m" << endl;
         drone1_homepos[0] = home_position.latitude_deg;
         drone1_homepos[1] = home_position.longitude_deg;
     });
+
+    
 
     while (true) {
         sleep_for(seconds(1));
@@ -201,12 +216,13 @@ int main() {
         cout << bearing << " degrees" << endl;
 
         if (distance_diff * 1000 >= distance_treshold) {
-            update_home(mavlink_passthrough1, drone2_pos[0], drone2_pos[1], home_altitude); // drone için home noktası güncellemesi
+            
 
-            auto mission_items_result = mission1.download_mission();
-            if (mission_items_result.first == Mission::Result::Success) {
-                auto mission_items = mission_items_result.second.mission_items; // MissionPlan içindeki mission_items'e eriş
-                update_waypoints(mission1, mission_items, distance_diff, bearing); // Waypoint'leri güncelle
+            auto mission_items_result = mission_raw1.download_mission();
+            if (mission_items_result.first == MissionRaw::Result::Success) {
+                auto mission_items = mission_items_result.second; // MissionPlan içindeki mission_items'e eriş
+                update_waypoints(mission_raw1, mission_items,drone2_pos_vector,drone1_homepos_vector); // Waypoint'leri güncelle
+                update_home(mavlink_passthrough1, drone2_pos[0], drone2_pos[1], home_altitude); // drone için home noktası güncellemesi
             }
         }
     }
